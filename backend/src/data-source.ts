3537ba1a0d50a2bd9f027f8d62479d1ac146be35
TaylonSopeletto12 <taylon@neoapp.com.br>
2022-08-06 22:44:42 -0300

    set credentials

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -6,7 +6,7 @@ import { Product } from "./entity/Product"
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DATABASE_HOST||  "localhost",
-    port: process.env.DATABASE_PORT || 5432,
+    port: Number(process.env.DATABASE_PORT) || 5432,
     username: process.env.DATABASE_USER || "postgres",
     password: process.env.DATABASE_PASSWORD || "postgres",
     database: process.env.DATABASE_NAME || "ecommerce",
